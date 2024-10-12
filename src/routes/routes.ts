d70abe939656c93668d1d31fccb31c6baecdcf71
Nvy <45800204+FF108@users.noreply.github.com>
2024-10-12 15:54:05 +0200

    (feat): init all routes from a single class

---
+++ src/routes/routes.ts
@@ -0,0 +1,10 @@
+import { Application } from "express";
+import characterRoutes from "./character.routes";
+
+class Routes {
+  constructor(app: Application) {
+    app.use("/character", characterRoutes);
+  }
+}
+
+export default Routes
