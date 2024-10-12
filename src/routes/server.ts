0278763ba91e23601f202310173fe60b8a6884be
Nvy <45800204+FF108@users.noreply.github.com>
2024-10-12 15:54:40 +0200

    (feat): create Server to handle express config and routes

---
+++ src/routes/server.ts
@@ -0,0 +1,15 @@
+import { Application } from "express";
+import morgan from "morgan";
+import Routes from "./routes"
+
+class Server {
+  constructor(app: Application) {
+    this.config(app)
+    new Routes(app)
+  }
+
+  private config(app: Application) {
+    app.use(morgan("tiny"))
+  }
+}
+export default Server;
