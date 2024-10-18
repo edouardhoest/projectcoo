76dd1f0b78a41c791dbe26d19914662e5ad6cc37
Nvy <45800204+FF108@users.noreply.github.com>
2024-10-18 15:31:26 +0200

    feat: create RouteExpress

--- src/routes/Route/RouteExpress.ts
+++ src/routes/Route/RouteExpress.ts
@@ -1,10 +1,15 @@
 import { Application } from "express";
+import Route from '../route'
 import characterRoutes from "./character.routes";
 
-class Routes {
+class RouteExpress implements Route<Application> {
   constructor(app: Application) {
+    this.init(app)
+  }
+
+  init(app: Application): void {
     app.use("/character", characterRoutes);
   }
 }
 
-export default Routes;
+export default RouteExpress;
