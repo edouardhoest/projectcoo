f8b3ae99f5385d6fa681de2c455919bedfcd88de
Korentin <korentin.kolaczek@gmail.com>
2024-10-18 15:37:04 +0200

    (feat): add node-json-db and db getter and setter

--- src/dto/Alignment.ts
+++ src/dto/Alignment.ts
@@ -4,4 +4,12 @@ import { Moral } from "../enums/Moral";
 export class Alignment {
   private moral: Moral;
   private order: Order;
+
+  public getMoral() : Moral{
+    return this.moral;
+  }
+
+  public getOrder() : Order {
+    return this.order;
+  }
 }
