b6c2bf7b4298073ffebd4f33b41f3a89f965ea7c
Nvy <45800204+FF108@users.noreply.github.com>
2024-10-18 15:17:50 +0200

    feat: create ExpressServer and make Server an interface

--- src/routes/server.ts
+++ src/routes/server.ts
@@ -1,16 +1,4 @@
-import { Application } from "express";
-import morgan from "morgan";
-import Routes from "./routes";
-
-class Server {
-  constructor(app: Application) {
-    this.config(app);
-    new Routes(app);
-  }
-
-  private config(app: Application) {
-    app.use(morgan("tiny"));
-  }
+export default interface Server {
+  config(): void;
+  listen(callback?: () => void): void;
 }
-
-export default Server;
