f755cf87a188ec2912e042c2623ea0abdf605b5c
Nvy <45800204+FF108@users.noreply.github.com>
2024-11-05 12:47:55 +0100

    (feat): try api request

--- src/repositories/CharacterRepositoryImpl.ts
+++ src/repositories/CharacterRepositoryImpl.ts
@@ -1,7 +1,6 @@
 import { Config, JsonDB } from "node-json-db";
 import { Character } from "../entity/Character";
 import { CharacterRepository } from "./CharacterRepository";
-import * as https from "node:https";
 
 export class CharacterRepositoryImpl implements CharacterRepository {
   private db = new JsonDB(new Config("database", true, false, "/"));
@@ -16,36 +15,13 @@ export class CharacterRepositoryImpl implements CharacterRepository {
     return await character;
   }
 
-  async retrieveClassInformation(): Promise<object> {
-    const url = "https://www.dnd5eapi.co/api/races";
+  async retrieveClassInformation() {
+    const res = await fetch('https://www.dnd5eapi.co/api/races');
+    const headerDate = res.headers && res.headers.get('date') ? res.headers.get('date') : 'no response date';
+    console.log('Status Code:', res.status);
+    console.log('Date in Response header:', headerDate);
 
-    return new Promise((resolve, reject) => {
-      const req = https.get(url, (res) => {
-        let data = "";
-
-        if (res.statusCode !== 200) {
-          reject(new Error(`Failed to get informations : ${res.statusCode}`));
-          res.resume();
-          return;
-        }
-
-        res.on("data", (chunk) => {
-          data += chunk;
-        });
-
-        res.on("end", () => {
-          try {
-            const jsonData = JSON.parse(data);
-            resolve(jsonData);
-          } catch (error) {
-            reject(new Error(`Error when parsing json`));
-          }
-        });
-      });
-
-      req.on("error", (error) => {
-        reject(new Error(`Erreur lors de la requête : ${error.message}`));
-      });
-    });
+    const species = await res.json();
+    console.log(species)
   }
 }
