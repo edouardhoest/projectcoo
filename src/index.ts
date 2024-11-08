cf4e55a4d2cdc8a26c42456eee91214d19eb320f
Nvy <45800204+FF108@users.noreply.github.com>
2024-11-08 12:29:57 +0100

    (fix): server crash without the env file

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import ExpressServer from "./routes/Server/ExpressServer";
 import Server from "./routes/Server";
 
-const app: Server = new ExpressServer(parseInt(process.env.SERVER_PORT ?? "") ?? 8080);
+const app: Server = new ExpressServer(parseInt(process.env.SERVER_PORT ?? "") || 8080);
 
 app.listen(() => console.log("All good !"));
